9eb5d89538e49f1b1947346cb46ddbf14891918b
Alice Mahtani <alice.mahtani@gmail.com>
2024-11-26 10:48:40 +0000

    add notes for other components

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 // Vowel Counter
 document.addEventListener("DOMContentLoaded", function () {
-    const countVowelsButton = document.getElementById("count-vowels");
-    const textInput = document.getElementById("text-input");
-    const vowelResult = document.getElementById("vowel-result");
+  const countVowelsButton = document.getElementById("count-vowels");
+  const textInput = document.getElementById("text-input");
+  const vowelResult = document.getElementById("vowel-result");
 
-    countVowelsButton.addEventListener("click", function () {
-        const text = textInput.value;
-        const vowelCount = countVowels(text);
-        if (vowelCount === 0) {
-            alert("No vowels detected, try again.");
-        } else {
-            vowelResult.textContent = `Number of vowels: ${vowelCount}`;
-        }
-    });
-
-    function countVowels(text) {
-        const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
-        let count = 0;
-        for (let char of text) {
-            if (vowels.includes(char)) {
-                count++;
-            }
-        }
-        return count;
+  countVowelsButton.addEventListener("click", function () {
+    const text = textInput.value;
+    const vowelCount = countVowels(text);
+    if (vowelCount === 0) {
+      alert("No vowels detected, try again.");
+    } else {
+      vowelResult.textContent = `Number of vowels: ${vowelCount}`;
     }
+  });
+
+  function countVowels(text) {
+    const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+    let count = 0;
+    for (let char of text) {
+      if (vowels.includes(char)) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  // expense tracker
+
+  //note taker
 });
-Dillon 
